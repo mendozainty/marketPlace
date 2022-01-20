94ef43a86f9ef63869dfefde6e3abf21914a6822
Inty Mendoza <intymendoza@gmail.com>
2022-01-20 12:10:13 -0300

    Update InstrumentInventory.sol

--- InstrumentInventory.sol
+++ InstrumentInventory.sol
@@ -1,8 +1,45 @@
 //SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.11;
-//pragma experimental ABIEncoderV2;
 
-contract MyTest {
+// OpenZeppelin Contracts v4.4.1 (access/Ownable.sol)
+
+abstract contract Ownable {
+    address private _owner;
+    mapping (address => uint) public balance;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    constructor() {
+        _transferOwnership(msg.sender);
+        balance[msg.sender];
+    }
+
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+    
+    modifier onlyOwner() {
+        require(owner() == msg.sender, "Ownable: caller is not the owner");
+        _;
+    }
+
+    function renounceOwnership() public virtual onlyOwner {
+        _transferOwnership(address(0));
+    }
+
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        _transferOwnership(newOwner);
+    }
+
+    function _transferOwnership(address newOwner) internal virtual {
+        address oldOwner = _owner;
+        _owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
+    }
+}
+
+contract MyTest is Ownable {
     struct Instrument {
         string instrument;
         string model;
@@ -19,9 +56,10 @@ contract MyTest {
     event newItem (string, string, uint, uint);
     event itemSold (string);
     event newQuant (string, uint);
+    event itemUnavail (string);
 
 
-   function setItem(string memory _instrument, string memory _model, uint _price, uint _quantity) public returns (bool) {
+   function setItem(string memory _instrument, string memory _model, uint _price, uint _quantity) public onlyOwner returns (bool) {
         Instrument memory newInstrument = Instrument(_instrument, _model, _price, _quantity, true);
         itemStat[_instrument] = newInstrument;
         instruments.push(Instrument(_instrument, _model, _price, _quantity, true)); 
@@ -31,51 +69,57 @@ contract MyTest {
         return true;       
     }
 
-    function isAvailable(string memory _instrument) internal returns (bool) {
-        require(itemStat[_instrument].available = true, "Item not available");
-        return true;
+    function isAvailable(string memory _instrument) internal view returns (bool) {
+        return itemStat[_instrument].available;
     }
 
-    function delItem(string memory _instrument) public returns (bool) {
-        isAvailable;
+    function setUnavail(string memory _instrument) public onlyOwner returns (bool) {
+        require(isAvailable(_instrument) == true, "Item not available");
         itemStat[_instrument].available = false;
+        emit itemUnavail(_instrument);
         return true;
     }
 
-    function getInventory() public view returns (Instrument[] memory _instruments) {
+    function getInventory() public view onlyOwner returns (Instrument[] memory _instruments) {
          _instruments = new Instrument[](itemCount); 
     
         for (uint i = 0; i < instruments.length; i++) {
-            Instrument storage _Instrument = instruments[i];
+            Instrument memory _Instrument = instruments[i];
             _instruments[i] = _Instrument;
         }
         return _instruments;
     }
 
-    function GetItemStat(string memory _instrument) public view returns (Instrument memory _Instrument) {
+    function GetItemStat(string memory _instrument) public view onlyOwner returns (Instrument memory _Instrument) {
         _Instrument = itemStat[_instrument];
         return _Instrument;
     }
 
     function sellItem(string memory _instrument) public returns (bool) {
-        Instrument storage _Instrument = itemStat[_instrument];
-        require(_Instrument.quantity != 0);
-        _Instrument.quantity -= 1;
+        require(isAvailable(_instrument) == true, "Item not available");
+                if (itemStat[_instrument].quantity == 1) {
+                    itemStat[_instrument].quantity -= 1; 
+                    setUnavail(_instrument);
+                    emit itemUnavail(_instrument);
+                } else {
+                    itemStat[_instrument].quantity -= 1;
+                }
         emit itemSold (_instrument);
+        
         return true;     
     }
 
     function setPrice(string memory _instrument, uint _newPrice) public returns (bool) {
-        Instrument storage _Instrument = itemStat[_instrument];
-        _Instrument.price = _newPrice;
+        require(isAvailable(_instrument) == true, "Item not available");
+        itemStat[_instrument].price = _newPrice;
         emit newPrice (_instrument, _newPrice);
         return true;
     }
 
     function addQuant(string memory _instrument, uint _quantityAdd) public returns (bool) {
-        Instrument storage _Instrument = itemStat[_instrument];
-        _Instrument.quantity += _quantityAdd;
-        emit newQuant (_instrument, _Instrument.quantity);
+        require(isAvailable(_instrument) == true, "Item not available");
+        itemStat[_instrument].quantity += _quantityAdd;
+        emit newQuant (_instrument, itemStat[_instrument].quantity);
         return true;
     }
 
