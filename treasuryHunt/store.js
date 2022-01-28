238f63f839d82c218b91b995963f202bad898bb1
Inty Mendoza <intymendoza@gmail.com>
2022-01-28 12:02:24 -0300

    jquery

--- treasuryHunt/store.js
+++ treasuryHunt/store.js
@@ -1,6 +1,25 @@
-var table = document.getElementById("cart-table");
+let _addToCart = document.getElementsByClassName("btn-addToCart")
+let productCard = []
 
-function deleteRow(){
-    var row = document.getElementById("table-row");
-    row.parentNode.removeChild(row);
+for(let i = 0; i < _addToCart.length; i++) {
+    _addToCart[i].addEventListener("click", addToCart)
 }
+function addToCart(event){
+    //let _mainContainer = document.getElementsById("cart-container")[0]
+    let _btn = event.target
+    let _btnParent = _btn.parentElement
+    let _btnGrandParent = _btn.parentElement.parentElement
+    let _itemName = _btnParent.children[1].innerText
+    let _itemPrice = _btnParent.children[2].innerText
+    let _select = document.getElementById("Color")
+    let _itemColor = _select.options[_select.selectedIndex].value
+    let _itemImage = _btnGrandParent.children[0].children[0].src
+    let _quantity = document.getElementById("Quantity").value
+    let itemPriceN = _itemPrice.replace(/\D/g, "")
+    let _subtotal = _quantity * itemPriceN
+    
+    productCard.push({_itemImage, _itemName, _itemColor, _itemPrice, _subtotal})
+    
+}
+
+console.log(productCard)
