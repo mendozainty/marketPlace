43f6d3130436039e08c050720acbb2d115c2f0d0
Inty Mendoza <intymendoza@gmail.com>
2022-01-31 23:04:34 -0300

    New site

---
+++ TreasureHuntPhp/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+    // banner owl carousel
+    $("#banner-area .owl-carousel").owlCarousel({
+        loop: true,
+        dots: true,
+        items: 1
+    });
+});
