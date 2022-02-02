1adb1484738dbcd9269e7769c06ec9e7842ba152
Inty Mendoza <intymendoza@gmail.com>
2022-02-02 11:27:06 -0300

    new-adds as poderosas

--- TreasureHuntPhp/index.js
+++ TreasureHuntPhp/index.js
@@ -6,4 +6,53 @@ $(document).ready(function(){
         dots: true,
         items: 1
     });
+
+    // as cores mais quentes owl carousel
+    $("#top-sale .owl-carousel").owlCarousel({
+        loop: true,
+        nav: true,
+        dots: false,
+        responsive:{
+            0:{
+                items: 1
+            },
+            600:{
+                items: 3
+            },
+            1000:{
+                items: 5
+            }
+        }
+    })
+
+    // isotope filter
+    var $grid = $(".grid").isotope({
+        itemSelector: '.grid-item',
+        layoutMode : 'fitRows'
+    });
+
+    // filter items on button click
+    $(".button-group").on("click", "button", function(){
+        var filterValue = $(this).attr('data-filter');
+        $grid.isotope({filter: filterValue});
+    })
+
+    // new-products owl-carousel
+    $("#new-products .owl-carousel").owlCarousel({
+        loop: true,
+        nav: false,
+        dots: true,
+        responsive:{
+            0:{
+                items: 1
+            },
+            600:{
+                items: 3
+            },
+            1000:{
+                items: 5
+            }
+        }
+    })
+
 });
