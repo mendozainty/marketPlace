78880f8960e184f9e551f29d883f068dac1f4688
Inty Mendoza <intymendoza@gmail.com>
2022-02-04 19:10:33 -0300

    product & cart

--- TreasureHuntPhp/index.js
+++ TreasureHuntPhp/index.js
@@ -79,4 +79,29 @@ $(document).ready(function(){
         }
     })
 
+    // qty button
+    let $qty_up = $(".qty .qty-up");
+    let $qty_down = $(".qty .qty-down");
+    //let $input = $(".qty .qty_input")
+
+    $qty_up.click(function(e){
+        let $input = $(`.qty_input[data-id='${$(this).data("id")}']`);
+        
+        if($input.val() >= 1 && $input.val() <= 9){
+            $input.val(function(i, oldvalue){
+                return ++oldvalue;
+            })
+        }
+    })
+
+    $qty_down.click(function(e){
+        let $input = $(`.qty_input[data-id='${$(this).data("id")}']`);
+        if($input.val() > 1 && $input.val() <= 10){
+            $input.val(function(i, oldvalue){
+                return --oldvalue;
+            })
+        }
+    })
+    
+
 });
